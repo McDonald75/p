2c8d3b99755f40aaf922f798e8d52055daf7ba40
McDonald75 <mcdonald.omondi@student.moringaschool.com>
2025-02-03 03:46:02 +0000

    added cors

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from flask_migrate import Migrate
 
 from .config import config
@@ -9,6 +10,8 @@ migrate = Migrate()
 
 def create_app(config_mode):
     app = Flask(__name__)
+    # CORS(app)
+    CORS(app)
     app.config.from_object(config[config_mode])
     # print(config.get('development'))
 
